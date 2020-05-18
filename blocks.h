c66a82e90c0950fbee7c5ad0ff4706e5637ed2ce
menganha <34135296+menganha@users.noreply.github.com>
2020-05-18 13:35:37 +0200

    Added personal config.

--- blocks.h
+++ blocks.h
@@ -1,19 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/	/*Post String*/	/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d", "", 0, 9},
+	/*Icon*/	/*Command*/  /*Post String*/	/*Update Interval*/	/*Update Signal*/
+    {" ", "~/scripts/i3_archUpdates", " ", 0, 11},
+
+    {" ", "INTERFACE=wlp5s0;export INTERFACE;~/scripts/i3_wifi", " ", 10, 0},
 	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", "", 30, 0},
+	{"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", " ", 30, 0},
 
-	{"", "~/bin/statusbar/volume", "", 0, 10},
+	{" ", "~/scripts/i3_volume"," ", 0, 10},
 
-	{"☀", "xbacklight | sed 's/\\..*//'", "", 0, 11},
+	{" ", "~/scripts/i3_backlight", " ", 0, 10},
 	
-	{"", "~/bin/statusbar/battery", "", 5, 0},
+	{" ", "~/scripts/i3_battery", " ", 10, 0},
 
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'", "", 5, 0},
+//	{" ", "sensors | awk '/^temp1:/{print $2\" \"}'", "", 10,0},
 
-	{"", "~/bin/statusbar/clock", "", 5, 0},
+	{" ", "~/scripts/clock", " ", 50, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
